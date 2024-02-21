1fe71a3fd595486a8fb71ed5145780dc4ae0efc1
Sanket Banait <53459669+Sanket1911@users.noreply.github.com>
2024-02-21 11:15:03 +0530

    Update App.js

--- public/src/App.js
+++ public/src/App.js
@@ -6,7 +6,6 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 export default function App() {
 
-  var api_token = "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";
   return (
     <BrowserRouter>
       <Routes>
